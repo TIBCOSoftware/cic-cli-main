fa19a7e74a58691d4b0e6c8efd51f62f684e6d5c
varpatil-tibco <varpatil@tibco.com>
2022-11-13 19:30:50 +0530

    Updated license and copyright text to clean-all.ts

--- src/commands/clean-all.ts
+++ src/commands/clean-all.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright 2022. TIBCO Software Inc.
+ * This file is subject to the license terms contained
+ * in the license file that is distributed with this file.
+ */
+
 import { flags } from '@oclif/command';
 import { ProfileConfig, ProfileConfigManager, secureStore, TCBaseCommand, ux } from '@tibco-software/cic-cli-core';
 
